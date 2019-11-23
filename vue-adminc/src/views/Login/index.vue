2438822fcd28e2bf1e281b27949e8cacf8c83d17
Vayne <434441168@qq.com>
2019-11-23 11:33:39 +0800

    初步构建项目

---
+++ vue-adminc/src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    这里是登录页
+    <el-button disabled>默认按钮</el-button>
+    <el-button type="primary" disabled>主要按钮</el-button>
+    <el-button type="success" disabled>成功按钮</el-button>
+    <el-button type="info" disabled>信息按钮</el-button>
+    <el-button type="warning" disabled>警告按钮</el-button>
+    <el-button type="danger" disabled>危险按钮</el-button>
+  </div>
+</template>
+
+<script>
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
